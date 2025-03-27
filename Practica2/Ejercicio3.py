36af4bbde576256e5e29635b7f6214c5dafd3932
Guido Olivero <102196175+Guidoolivero@users.noreply.github.com>
2025-03-27 22:42:27 +0000

    cambios

--- Practica2/Ejercicio3.py
+++ Practica2/Ejercicio3.py
@@ -1,7 +1,21 @@
-"Escribir un programa que muestre, de a 10 numeros por linea y separados por 1 blanco, todos los numeros entre 100 y 1000 que sean divisibles por 5 y 6"
+"Escribir un programa que muestre, de a 10 numeros por linea y separados por un blanco, todos los numeros entre 100 y 1000 que sean divisibles por 5 y 6"
+
+
 
 def programa():
-    
+
+    count = 0
+
+    for num in range(100, 1001):
+
+        if num % 5 == 0 and num % 6 == 0:
+            print(num, end=" ")
+
+            count += 1
+
+        
+
+programa()
 
 
 
