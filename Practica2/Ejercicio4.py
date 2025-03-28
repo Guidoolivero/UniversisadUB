c7b47db4873b44437af4c68e0fdff1f8d389dbe2
Guido Olivero <102196175+Guidoolivero@users.noreply.github.com>
2025-03-28 00:13:47 +0000

    cambio

--- Practica2/Ejercicio4.py
+++ Practica2/Ejercicio4.py
@@ -1,2 +1,21 @@
 "Construir un programa que permita multiplicar dos numeros enteros positivos empleando el metodo denomindado miltiplicacion rusa."
 "Este metodo permite calcular el producto de N por M de la siguiente forma"
+
+
+a = int(input(print("Inserte el multiplicador: ")))
+b = int(input(print("Inserte el multiplicando: ")))
+
+
+
+def MultiplicacionRusa(a, b):
+    
+    resultado = a * b
+    return resultado
+
+print(f"{a} * {b} = {MultiplicacionRusa(a,b)}")
+             
+    
+    
+    
+    
+
