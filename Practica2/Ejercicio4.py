6160daa66f0e1a6134a34d640bad9f99a05bf4ad
Guido Olivero <102196175+Guidoolivero@users.noreply.github.com>
2025-04-01 15:34:21 +0000

    cambio

--- Practica2/Ejercicio4.py
+++ Practica2/Ejercicio4.py
@@ -2,17 +2,27 @@
 "Este metodo permite calcular el producto de N por M de la siguiente forma"
 
 
-a = int(input(print("Inserte el multiplicador: ")))
-b = int(input(print("Inserte el multiplicando: ")))
 
 
 
-def MultiplicacionRusa(a, b):
+def MultiplicacionRusa():
+   
+   multiplicando = int(input(print("Inserte el multiplicador: ")))
+   multiplicador = int(input(print("Inserte el multiplicando: ")))
+   acum = 0 
     
-    resultado = a * b
-    return resultado
+   while multiplicando >= 1:
+      if multiplicando % 2 != 0:
+         acum = acum + multiplicador
+         multiplicando = multiplicando // 2
+         multiplicador = multiplicador * 2
+         
+   print(f"La respuesta es: {acum}")
+         
 
-print(f"{a} * {b} = {MultiplicacionRusa(a,b)}")
+         
+          
+MultiplicacionRusa()
              
     
     
