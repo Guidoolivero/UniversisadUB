a96659b14cbf635f17509cdf423dc46c4b50188e
Guido Olivero <102196175+Guidoolivero@users.noreply.github.com>
2025-04-22 20:14:35 -0300

    Update Ejercicio7.py

--- Practicas Python/Practica3/Ejercicio7.py
+++ Practicas Python/Practica3/Ejercicio7.py
@@ -4,3 +4,43 @@ Escribir un programa que ingrese un número telefónico como un string y convier
 caracteres a el dígito correspondiente, ejemplo:
 1-800-FLOWERS ---> 1-800-3569377
 """
+"""
+
+Escribir un programa que ingrese un número telefónico como un string y convierta los
+caracteres a el dígito correspondiente, ejemplo:
+1-800-FLOWERS ---> 1-800-3569377
+
+"""
+
+
+def numero_telefonico(letra):
+    
+    numeros = {
+        2: "abc",
+        3: "def",
+        4: "ghi",
+        5: "jkl",
+        6: "mno",
+        7: "pqrs",
+        8: "tuv",
+        9: "wxyz"
+    }
+    
+    for numero, letras in numeros.items():
+        if letra.lower() in letras:
+            return str(numeros)
+         
+    return letra
+
+
+
+    resultado = ""
+    
+    for caracter in texto: 
+        resultado += numero_telefonico(caracter)
+    
+    return resultado
+
+telefono = input("Ingrese el numero: ")
+print("Numeros convertidos: ", numero_telefonico(telefono))
+
